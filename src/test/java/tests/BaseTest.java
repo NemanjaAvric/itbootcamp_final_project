aaa3efbdf1686539eac257c436e254e76b52e415
NemanjaAvric <avricnemanja7@gmail.com>
2023-01-31 18:34:26 +0100

    Initial commit

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait webDriverWait;
+    protected final String BASEURL = "https://vue-demo.daniel-avellaneda.com";
+
+    @BeforeClass
+    public void beforeClass() {
+        System.setProperty("webriver.driver.chromedriver", "C:\\Nova fascikla\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(30));
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.get(BASEURL);
+        driver.manage().window().fullscreen();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
